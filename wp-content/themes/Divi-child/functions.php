704c74170327958326ca957bf6f587f8fa73a127
alainejumapao1008 <alainelovinojumapao@gmail.com>
2016-08-06 22:45:55 +0800

    firts version of the application
    first created version

---
+++ wp-content/themes/Divi-child/functions.php
@@ -0,0 +1,6 @@
+<?php
+//*Code goes here
+add_action( 'wp_enqueue_scripts', 'my_enqueue_assets' );
+function my_enqueue_assets() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
+}
